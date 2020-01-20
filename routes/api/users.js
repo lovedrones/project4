95890c6d5e9374b6edcfaf60e33b1fb74fd5b2df
lovedrones <JuanTron@juans-mbp.attlocal.net>
2020-01-19 23:54:01 -0600

    signin, sign up, and out working. add auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const { 
      signup, 
      signin,
-     signout, 
-     requireSignin 
+     signout,
+     requireSignin
     } = require('../../controllers/users');
 
 const { userSignupValidator } = require("../../validator");
@@ -16,8 +16,6 @@ router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
 router.get("/signout", signout);
 
-router.get('/hello', requireSignin, (req, res) => {
-    res.send("hello there");
-});
+
 
 module.exports = router;
