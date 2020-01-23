be9d3cc95321c55a7d184ba8b1bed94c233cd531
lovedrones <JuanTron@juans-mbp.attlocal.net>
2020-01-22 21:39:58 -0600

    user profile read and update. before routing pages

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
-import { Route } from 'react-router-dom';
 
-
-//import routes
-
-// const authRoutes = require("../routes/api/auth");
-
-
-//db
-
-//middlewares
-
-// routes middleware
-// app.use("/api", authRoutes);
-
-function App() {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  );
-}
-
-// ReactDOM.render(<App />, document.querySelector('#app'));
+const App = () => (<div>Hello from Juan Lo</div>)
 
 export default App;
