0b83943cd3d7a50ecb86aeaa85f01b46c4edc2b3
lovedrones <JuanTron@Juans-MacBook-Pro.local>
2020-01-23 15:37:29 -0600

    user signin

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -74,7 +74,7 @@ const Signup = () => {
     return (
         <Layout
             title="Signup"
-            description="Signup to Node React E-commerce App"
+            description="Signup for Millions To Juan"
             className="container col-md-8 offset-md-2"
         >
             {showSuccess()}
