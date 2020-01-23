5af78e4408e81f4bcb13f23b5f7935f5ba318085
lovedrones <JuanTron@Juans-MacBook-Pro.local>
2020-01-23 11:34:06 -0600

    Signup Form handleChange

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from "../core/Layout";
 
 const Signin = () => (
-     <Layout title="Signin" description="Signin to Node React E-commerce App">
+     <Layout title="Signin" description="Signin to Millions to Juan">
 .....
      </Layout>
 );
